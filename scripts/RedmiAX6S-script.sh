7659547901272057f919cea5f67aefd00d462822
dolphin738 <91973916+dolphin738@users.noreply.github.com>
2026-03-11 02:49:58 +0800

    Update RedmiAX6S-script.sh

--- scripts/RedmiAX6S-script.sh
+++ scripts/RedmiAX6S-script.sh
@@ -8,7 +8,7 @@ sed -i "s#_('Firmware Version'), (L\.isObject(boardinfo\.release) ? boardinfo\.r
                 ? boardinfo.release.description + ' / '\n \
                 : '') + (luciversion || '') + ' / ',\n \
             E('a', {\n \
-                href: 'https://github.com/laipeng668/openwrt-ci-roc/releases',\n \
+                href: 'https://github.com/dolphin738/openwrt-ci-roc/releases',\n \
                 target: '_blank',\n \
                 rel: 'noopener noreferrer'\n \
                 }, [ 'Built by dolphin738 $(date "+%Y-%m-%d %H:%M:%S")' ])\n \
